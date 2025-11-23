c99b8f343bcd6900906dff79dc045ead56653a4c
Vinicius Italo da Cruz <viniciusidacruz@hotmail.com>
2025-11-23 00:37:00 -0300

    feat: initialize Next.js project with layout and styling setup

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title:
+    "Task Mental | Sua plataforma de mapas mentais para desenvolvimento de software frontend",
+  description:
+    "Task Mental é uma plataforma de mapas mentais para desenvolvimento de software frontend. Ele ajuda você a criar, gerenciar e compartilhar mapas mentais para desenvolvimento de software frontend.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
